a04e6f98ccd1b1993db7003e8865f29a86724991
dothanhtruc <trucdo0411@gmail.com>
2023-05-09 22:08:04 +0700

    18. Áp dụng mô hình MVC

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,13 @@
 import expess from "express";
+import homeController from "../controllers/homeController";
 
 let router = expess.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send('Hello world dothanhtruc')
-    });
+    router.get("/", homeController.getHomePage);
+    router.get("/about", homeController.getAboutPage);
+
+
 
     return app.use("/", router);
 }
